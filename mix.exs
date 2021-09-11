e80d5204553a16faa95e241091971f7c5c2f7d6c
bryan <hi@braynm.com>
2021-09-11 10:59:05 +0800

    add the following deps in our `mix.exs:`   - mox   - tesla   - hackney   - floki

--- mix.exs
+++ mix.exs
@@ -44,7 +44,11 @@ defmodule LiveViewExampleUsage.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:mox, "~> 1.0", only: :test},
+      {:tesla, "~> 1.4"},
+      {:hackney, "~> 1.17"},
+      {:floki, ">= 0.30.0", only: :test}
     ]
   end
 
