04ddfb10978e9b1eee05760e3644f979419a5559
bryan <hi@braynm.com>
2021-09-11 12:21:07 +0800

    remove `LiveViewExampleUsage.Repo` to our supervision tree

--- lib/live_view_example_usage/application.ex
+++ lib/live_view_example_usage/application.ex
@@ -8,7 +8,7 @@ defmodule LiveViewExampleUsage.Application do
   def start(_type, _args) do
     children = [
       # Start the Ecto repository
-      LiveViewExampleUsage.Repo,
+      # LiveViewExampleUsage.Repo,
       # Start the Telemetry supervisor
       LiveViewExampleUsageWeb.Telemetry,
       # Start the PubSub system
